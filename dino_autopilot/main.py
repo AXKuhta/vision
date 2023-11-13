07044b277dd6c08c73c4340e3ebfa22229129984
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-11-13 19:48:49 +0800

    Grab only the relevant portion of the screen, 20ms -> 0.5ms

--- dino_autopilot/main.py
+++ dino_autopilot/main.py
@@ -12,8 +12,11 @@ def visual_debug(image):
 	plt.show()
 
 # Захватить экран и бинаризировать
-def binary_frame():
-	shot = sct.grab(sct.monitors[0])
+def binary_frame(roi=None):
+	if roi is None:
+		roi = sct.monitors[0]
+
+	shot = sct.grab(roi)
 	h, w = shot.height, shot.width
 
 	# rgb = геттер (https://github.com/BoboTiG/python-mss/blob/main/src/mss/screenshot.py)
@@ -24,17 +27,40 @@ def binary_frame():
 
 	return bin
 
-# Baseline detect
-baseline = binary_frame().sum(1).argmax()
-roi = slice(baseline - 110, baseline - 10)
+def roi_detect():
+	frame = binary_frame()
+	baseline = frame.sum(1).argmax()
+	first_col = None
+	last_col = None
+
+	for i, v in enumerate(frame[baseline]):
+		if v:
+			last_col = i
+			if first_col is None:
+				first_col = i
+
+	print("Game detect:", first_col, last_col)
+
+	# Предположим, что соотношение сторон всегда 7/30
+	width = last_col - first_col
+	height = round(width * 7/30)
+
+	return {
+		"width": width,
+		"height": height,
+		"left": first_col,
+		"top": baseline - height,
+		"mon": 0
+	}
+
+roi = roi_detect()
 
 while True:
 	start = perf_counter()
-	frame = binary_frame()
-	game = frame[roi]
+	game = binary_frame(roi)
 
 	# 1134 is the last pixel of the dino
-	if np.any(game.sum(0)[1160:1260]):
+	if np.any(game.sum(0)[100:150] > 10):
 		pyautogui.press("space")
 
 	elapsed = perf_counter() - start
