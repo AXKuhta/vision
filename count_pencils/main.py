03e969603f320cb643afa05e9c92b9900c88a307
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-11-22 13:52:48 +0800

    Proper pencil counter

--- count_pencils/main.py
+++ count_pencils/main.py
@@ -21,8 +21,19 @@ def binarize(img):
 
 	return binary
 
+# Повернуть облако точек против часовой стрелки на angle (в радианах) и найти соотношение ширины к высоте
+def tilt_and_calc_ratio(rows, cols, angle):
+	rows_tilt = -np.sin(angle)*cols + np.cos(angle)*rows
+	cols_tilt = np.cos(angle)*cols + np.sin(angle)*rows
+
+	row_span_tilt = rows_tilt.max() - rows_tilt.min()
+	col_span_tilt = cols_tilt.max() - cols_tilt.min()
+
+	return col_span_tilt / row_span_tilt
+
 # Найти длинные объекты на бинарном изображении
 def detect_long_objects(binary):
+	pencils = []
 
 	# Найти объекты
 	field, count = label(binary)
@@ -31,11 +42,50 @@ def detect_long_objects(binary):
 	area = np.bincount(field.flatten())
 	largest = area.argsort()
 
-	for i in largest[-4:-1]:
+	# Пробежка по объектам
+	# - Пропустить фон
+	# - Пропустить всякую мелочь
+	for i, s in enumerate(area):
+		if i == 0:
+			continue
+
+		if s < 200000:
+			continue
+
+		# Получить точки
+		rows, cols = np.where(field == i)
+
+		row_span = rows.max() - rows.min()
+		col_span = cols.max() - cols.min()
+
+		# Попробовать отгадать угол
+		# Будет ~0 для лежащих горизонтально карандашей и ~pi/2 для лежащих вертикально карандашей
+		ratio = row_span / col_span
+		angle = np.arctan(ratio)
+
+		# Попробовать довернуть карандаш против часовой стрелки... и по часовой стрелке
+		# Из одного ratio нельзя однозначно понять, куда его надо доворачивать
+		wide_factor_a = tilt_and_calc_ratio(rows, cols, 0)
+		wide_factor_b = tilt_and_calc_ratio(rows, cols, angle)
+		wide_factor_c = tilt_and_calc_ratio(rows, cols, -angle)
+
+		# Признать это карандашом, если он более чем в 10 раз длиннее, чем он выше
+		if wide_factor_b > 10 or wide_factor_c > 10:
+			pencils.append(i)
+			pencil = True
+		else:
+			pencil = False
+
+		"""
 		plt.figure()
-		plt.title(f"Area: {area[i]}")
+		plt.title(f"Angle: {angle}\nPencil? {pencil}")
 		plt.imshow(field == i)
 		plt.show()
+		"""
+
+	return len(pencils)
+
+total = 0
 
 for filename in glob("images/*.jpg"):
 	img = cv2.imread(filename)
@@ -45,4 +95,10 @@ for filename in glob("images/*.jpg"):
 
 	#visual_debug(img)
 	binary = binarize(img)
-	detect_long_objects(binary)
+	long_count = detect_long_objects(binary)
+
+	print(f"{filename}\t{long_count} pencils")
+
+	total += long_count
+
+print(total, "pencils total")
